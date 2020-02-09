7bd230a95afa4cd946883b929edfc5714ba4a4d3
vk hooda <er.vkhooda24@gmail.com>
2020-02-09 01:20:04 -0500

    InvalidImmutabilyException issue fix which is happening due to httpClientEngine(frozen instance) when passing to Ktor's HttpClient.

--- shared/src/androidMain/kotlin/com/vkhooda24/HttpClientEngine.kt
+++ shared/src/androidMain/kotlin/com/vkhooda24/HttpClientEngine.kt
@@ -1,7 +1,10 @@
 package com.vkhooda24
 
+import com.vkhooda24.knowyourcountry.model.Country
+import com.vkhooda24.service.CountryService
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
+import kotlinx.coroutines.runBlocking
 import okhttp3.logging.HttpLoggingInterceptor
 
 actual object HttpClientEngine {
---
+++ shared/src/iosX64Main/kotlin/com/vkhooda24/CountryList.kt
@@ -0,0 +1,5 @@
+package com.vkhooda24
+
+/**
+ * Created by Vikram Hooda on 2020-02-08.
+ */
